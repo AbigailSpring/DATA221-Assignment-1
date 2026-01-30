1aaa94dfc2fb46c294c7b7eb9a6511e1fedba2b3
Abigail Spring <abigail.spring@ucalgary.ca>
2026-01-29 17:11:06 -0700

    Initial commit

--- DATA221-Assignment1-Problem1-ControlledMultiplicationLoop.py
+++ DATA221-Assignment1-Problem1-ControlledMultiplicationLoop.py
@@ -6,7 +6,4 @@ while factorial_product_until_threshold_value <= multiplication_threshold_value:
     factorial_product_until_threshold_value *= integer_that_exceeds_threshold_value
 print(f"The threshold value is {multiplication_threshold_value}.")
 print(f"The final product is {factorial_product_until_threshold_value}.")
-print(
-    f"The integer that caused the product to exceed the threshold was "
-    f"{integer_that_exceeds_threshold_value}."
-)
+print(f"The integer that caused the product to exceed the threshold was " f"{integer_that_exceeds_threshold_value}.")
--- DATA221-Assignment1-Problem8-PandaDataFrameWithComputedColumn.py
+++ DATA221-Assignment1-Problem8-PandaDataFrameWithComputedColumn.py
@@ -5,9 +5,6 @@ data_dict = {
     "measurement_value": [3.1, 4.2, 1.5, 6.3],
     "quantity": [800, 150, 400, 210]
 }
-
-df = pd.DataFrame(data_dict)
-
-df["total_value"] = df["category_id"] * df["quantity"]
-
-print(df)
+modified_data_dict_into_data_frame = pd.DataFrame(data_dict)
+modified_data_dict_into_data_frame["total_value"] = modified_data_dict_into_data_frame["category_id"] * modified_data_dict_into_data_frame["quantity"]
+print(modified_data_dict_into_data_frame)
